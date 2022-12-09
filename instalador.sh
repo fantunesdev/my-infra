6b9a9f619b7c60356148dbb1f38fa7975c6fd891
Fernando Antunes <f.antunes84@gmail.com>
2022-12-09 16:49:23 -0300

    Pyenv

--- instalador.sh
+++ instalador.sh
@@ -7,11 +7,11 @@ sudo apt install -y gparted tilix keepassxc alacarte grub-customizer gsmartcontr
 
 flatpak install -y com.anydesk.Anydesk com.discordapp.Discord com.getpostman.Postman com.github.tchx84.Flatseal com.snes9x.Snes9x org.avidemux.Avidemux org.chromium.Chromium org.chromium.Chromium.Codecs org.duckstation.DuckStation org.flameshot.Flameshot org.gimp.GIMP org.inkscape.Inkscape org.libretro.RetroArch org.onlyoffice.desktopeditors org.qbittorrent.qBittorrent Dorg.signal.Signal rest.insomnia.Insomnia
 
-### SETAR PROGRAMAS PADRÃO ###
+######## SETAR PROGRAMAS PADRÃO ########
 sudo update-alternatives --config x-terminal-emulator
 xdg-mime default nemo.desktop inode/directory application/xgnome-saved-search
 
-### VIM ###
+######## VIM ########
 # Instalação do tema dracula
 mkdir -p ~/.vim/pack/themes/start
 cd ~/.vim/pack/themes/start
@@ -21,14 +21,15 @@ echo -e 'packadd! dracula\nsyntax enable\ncolorscheme dracula' >> ~/.vim/vimrc
 echo -e 'set number wrap title tabstop=4 softtabstop=4\n' >> ~/.vim/vimrc
 
 
-### BRAVE BROWSER ###
+######## BRAVE BROWSER ########
 sudo apt install -y apt-transport-https curl
 curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 apt update
 apt install brave-browser
 
-### CUSTOMIZAÇÕES DO TERMINAL ###
+
+######## CUSTOMIZAÇÕES DO TERMINAL ########
 BASHRC_FILE="$HOME/.bashrc"
 
 echo >> $BASHRC_FILE
@@ -38,7 +39,50 @@ echo 'echo export PSGIT='\''\e[1;35m\u@\h\e[m:\e[1;34m\w\e[m$(__git_ps1)\e[m\]\n
 echo 'export PSTRAD='\''\e[1;35m\u@\h\e[m:\e[1;34m\w\e[m$(__git_ps1)\e[m\]$ '\' >> $BASHRC_FILE
 echo >> $BASHRC_FILE
 
-### ASDF ###
+
+######## PYTHON ########
+
+# Instalação do PYENV
+cd /tmp
+export PYENV_GIT_TAG=v2.2.5
+curl https://pyenv.run | bash
+echo 'export PATH=$PATH:~/.pyenv/bin' >> ~/.bashrc
+exec bash
+sudo apt-get install -y python-tk python3-tk tk-dev
+pyenv update
+
+# Instalação do python
+
+LATEST_PYTHON=$(pyenv latest 3)
+IFS='.'
+read -a LATEST_PYTHON_SPLITTED_STRING <<< $LATEST_PYTHON
+unset IFS
+HIGHER_VERSION=$(("${LATEST_PYTHON_SPLITTED_STRING[1]}"))
+VERSION=$(($HIGHER_VERSION - 3))
+
+while ((VERSION <= HIGHER_VERSION)); do
+    pyenv install "3.$VERSION:latest"
+    pyenv install "3.$VERSION-dev"
+    let VERSION++
+done
+
+pyenv install anaconda3-2022:latest
+pyenv global $LATEST_PYTHON
+
+# Configurações
+pip install --upgrade pip
+pip install pipx
+pipx install poetry
+poetry config virtualenvs.in-project = true
+
+
+######## PYCHARM ########
+# PYCHARM_RELEASE='pycharm-community'
+PYCHARM_RELEASE='pycharm-professional'
+sudo snap install $PYCHARM_RELEASE --classic
+
+
+######## ASDF ########
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.10.2
 echo '. $HOME/.asdf/asdf.sh' >> $BASHRC_FILE
 echo '. $HOME/.asdf/completions/asdf.bash' >> $BASHRC_FILE
@@ -47,40 +91,28 @@ asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 sudo apt update
 sudo apt install python3 g++ make python3-pip
 
-### PYTHON ###
-asdf install python 3.10.8
-asdf install python 3.10-dev
-asdf install python latest
-asdf install python 3.11-dev
 
-### PYENV ###
-cd /tmp
-export PYENV_GIT_TAG=v2.2.5
-curl https://pyenv.run | bash
-echo 'export PATH=$PATH:~/.pyenv/bin' >> ~/.bashrc
+######## VSCODE ########
+sudo apt install -y code
 
-### PYCHARM ###
-# PYCHARM_RELEASE='pycharm-community'
-PYCHARM_RELEASE='pycharm-professional'
-sudo snap install $PYCHARM_RELEASE --classic
+######## SGBDs ########
+sudo apt install -y postgresql postgresql-contrib
+sudo apt install -y mysql-server mysql-client libmysqlclient-dev
 
-### SGBDs ###
-sudo apt install mysql-server mysql-client libmysqlclient-dev
-
-### VAULT ###
+######## VAULT ########
 cd /tmp
 curl https://releases.hashicorp.com/vault/1.4.0/vault_1.4.0_linux_amd64.zip -o vault.zip
 unzip vault.zip
 sudo mv vault /usr/local/bin
 vault -autocomplete-install
 
-### VAULTCTL ###
+######## VAULTCTL ########
 cd /tmp
 git clone git@github.com:fantunesdev/vaultctl.git
 cd vaultctl
 sudo ./install.sh
 
-### YouTube-DLG ###
+######## YouTube-DLG ########
 curl http://ubuntu.mirrors.tds.net/ubuntu/pool/universe/t/twodict/python-twodict_1.2-1_all.deb -o /tmp/python-twodict_1.2-1_all.deb
 curl http://ppa.launchpad.net/nilarimogard/webupd8/ubuntu/pool/main/y/youtube-dlg/youtube-dlg_0.4-1~webupd8~disco0_all.deb -o /tmp/youtube-dlg_0.4-1~webupd8~disco0_all.deb
 sudo apt install /tmp/python-twodict_1.2-1_all.deb /tmp/youtube-dlg_0.4-1~webupd8~disco0_all.deb
